f63c81314af8ea20040f6acaae30215d015ef628
Виктор Горбань <VictorGorban1999@yandex.ru>
2018-03-18 23:22:53 +0300

    Просто пишу код. Стою перед инициализацией таблицы результата.

--- Lab2 CSharp/Form1.Designer.cs
+++ Lab2 CSharp/Form1.Designer.cs
@@ -28,13 +28,156 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.PBBefore = new System.Windows.Forms.PictureBox();
+            this.LabelAfter = new System.Windows.Forms.Label();
+            this.LabelBefore = new System.Windows.Forms.Label();
+            this.ButtonLoad = new System.Windows.Forms.Button();
+            this.ButtonProcess = new System.Windows.Forms.Button();
+            this.ProgressBar = new System.Windows.Forms.ProgressBar();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.PBAfter = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.PBBefore)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PBAfter)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // PBBefore
+            // 
+            this.PBBefore.BackColor = System.Drawing.SystemColors.Control;
+            this.PBBefore.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PBBefore.Image = global::Lab2_CSharp.Properties.Resources.ru_default_large_default;
+            this.PBBefore.Location = new System.Drawing.Point(0, 0);
+            this.PBBefore.Name = "PBBefore";
+            this.PBBefore.Size = new System.Drawing.Size(462, 462);
+            this.PBBefore.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.PBBefore.TabIndex = 0;
+            this.PBBefore.TabStop = false;
+            // 
+            // LabelAfter
+            // 
+            this.LabelAfter.AutoSize = true;
+            this.LabelAfter.Font = new System.Drawing.Font("Sitka Small", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LabelAfter.Location = new System.Drawing.Point(662, 9);
+            this.LabelAfter.Name = "LabelAfter";
+            this.LabelAfter.Size = new System.Drawing.Size(58, 23);
+            this.LabelAfter.TabIndex = 2;
+            this.LabelAfter.Tag = "";
+            this.LabelAfter.Text = "После";
+            // 
+            // LabelBefore
+            // 
+            this.LabelBefore.AutoSize = true;
+            this.LabelBefore.Font = new System.Drawing.Font("Sitka Small", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.LabelBefore.Location = new System.Drawing.Point(212, 9);
+            this.LabelBefore.Name = "LabelBefore";
+            this.LabelBefore.Size = new System.Drawing.Size(31, 23);
+            this.LabelBefore.TabIndex = 3;
+            this.LabelBefore.Tag = "";
+            this.LabelBefore.Text = "До";
+            // 
+            // ButtonLoad
+            // 
+            this.ButtonLoad.Font = new System.Drawing.Font("Sitka Text", 13F);
+            this.ButtonLoad.Location = new System.Drawing.Point(12, 410);
+            this.ButtonLoad.Name = "ButtonLoad";
+            this.ButtonLoad.Size = new System.Drawing.Size(113, 33);
+            this.ButtonLoad.TabIndex = 4;
+            this.ButtonLoad.Text = "Загрузить";
+            this.ButtonLoad.UseVisualStyleBackColor = true;
+            this.ButtonLoad.Click += new System.EventHandler(this.ButtonLoad_Click);
+            // 
+            // ButtonProcess
+            // 
+            this.ButtonProcess.Font = new System.Drawing.Font("Sitka Text", 13F);
+            this.ButtonProcess.Location = new System.Drawing.Point(316, 410);
+            this.ButtonProcess.Name = "ButtonProcess";
+            this.ButtonProcess.Size = new System.Drawing.Size(126, 33);
+            this.ButtonProcess.TabIndex = 5;
+            this.ButtonProcess.Text = "Обработать";
+            this.ButtonProcess.UseVisualStyleBackColor = true;
+            this.ButtonProcess.Click += new System.EventHandler(this.ButtonProcess_Click);
+            // 
+            // ProgressBar
+            // 
+            this.ProgressBar.Location = new System.Drawing.Point(470, 410);
+            this.ProgressBar.Name = "ProgressBar";
+            this.ProgressBar.Size = new System.Drawing.Size(429, 23);
+            this.ProgressBar.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.Controls.Add(this.PBBefore);
+            this.panel1.Location = new System.Drawing.Point(12, 34);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(430, 370);
+            this.panel1.TabIndex = 7;
+            // 
+            // panel2
+            // 
+            this.panel2.AutoScroll = true;
+            this.panel2.Controls.Add(this.PBAfter);
+            this.panel2.Location = new System.Drawing.Point(470, 35);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(430, 370);
+            this.panel2.TabIndex = 8;
+            // 
+            // PBAfter
+            // 
+            this.PBAfter.BackColor = System.Drawing.SystemColors.Control;
+            this.PBAfter.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.PBAfter.Image = ((System.Drawing.Image)(resources.GetObject("PBAfter.Image")));
+            this.PBAfter.Location = new System.Drawing.Point(0, 0);
+            this.PBAfter.Name = "PBAfter";
+            this.PBAfter.Size = new System.Drawing.Size(462, 462);
+            this.PBAfter.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.PBAfter.TabIndex = 1;
+            this.PBAfter.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.ClientSize = new System.Drawing.Size(911, 469);
+            this.Controls.Add(this.ProgressBar);
+            this.Controls.Add(this.ButtonProcess);
+            this.Controls.Add(this.ButtonLoad);
+            this.Controls.Add(this.LabelBefore);
+            this.Controls.Add(this.LabelAfter);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.panel2);
+            this.MinimumSize = new System.Drawing.Size(600, 300);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResizeEnd += new System.EventHandler(this.Form1_ResizeEnd);
+            this.SizeChanged += new System.EventHandler(this.Form1_SizeChanged);
+            ((System.ComponentModel.ISupportInitialize)(this.PBBefore)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.PBAfter)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox PBBefore;
+        private System.Windows.Forms.Label LabelAfter;
+        private System.Windows.Forms.Label LabelBefore;
+        private System.Windows.Forms.Button ButtonLoad;
+        private System.Windows.Forms.Button ButtonProcess;
+        private System.Windows.Forms.ProgressBar ProgressBar;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox PBAfter;
     }
 }
 
--- Lab2 CSharp/Form1.cs
+++ Lab2 CSharp/Form1.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics.CodeAnalysis;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -10,11 +11,277 @@ using System.Windows.Forms;
 
 namespace Lab2_CSharp
 {
-    public partial class Form1 : Form
+    public partial class Form1: Form
     {
+        private double relationOfWidth;
+        private double relationOfDistanceBetweenPictureBoxes;
+        private double relationOfHeight;
+        private int initDistanceBetweenPictureboxes;
+        private int distanceBetweenPictureboxAndButton;
+
         public Form1()
         {
             InitializeComponent();
         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            initDistanceBetweenPictureboxes       = PBAfter.Left   - PBBefore.Right;
+            distanceBetweenPictureboxAndButton    = ButtonLoad.Top - PBBefore.Bottom;
+            relationOfDistanceBetweenPictureBoxes = (double) initDistanceBetweenPictureboxes / Width;
+            relationOfWidth = ((double) PBBefore.Width / Width - relationOfDistanceBetweenPictureBoxes) *
+                              1.07; // 1.07 - костыль. Неплохо работает.
+            relationOfHeight = (double) PBBefore.Height / Height;
+        }
+
+        private void Form1_SizeChanged(object sender, EventArgs e)
+        {
+            PBBefore.Size = new Size((int) (Width * relationOfWidth), (int) (Height * relationOfHeight));
+            PBAfter.Left  = (int) (PBBefore.Right + Width * relationOfDistanceBetweenPictureBoxes);
+            PBAfter.Size  = PBBefore.Size;
+
+            ButtonLoad.Top = ButtonProcess.Top =
+                ProgressBar.Top = PBBefore.Bottom + distanceBetweenPictureboxAndButton;
+            ButtonProcess.Left = PBBefore.Right - ButtonProcess.Width;
+
+            ProgressBar.Left  = PBAfter.Left;
+            ProgressBar.Width = PBAfter.Width;
+
+            LabelBefore.Left = (PBBefore.Left + PBBefore.Right) / 2 - LabelBefore.Width / 2;
+            LabelAfter.Left  = (PBAfter.Left  + PBAfter.Right)  / 2 - LabelAfter.Width  / 2;
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            //MessageBox.Show(Size.ToString());
+        }
+
+        private void ButtonLoad_Click(object sender, EventArgs e)
+        {
+            var dialog = new OpenFileDialog
+            {
+                Filter =
+                    @"Файлы изображений (*.gif; *.jpg; *.jpeg; *.bmp; *.wmf; *.png)|*.gif;*.jpg;*.jpeg;*.bmp;*.wmf;*.png",
+                Multiselect = false
+            };
+
+            if (! (dialog.ShowDialog() is DialogResult.OK))
+            {
+                return;
+            }
+
+            try
+            {
+                var image = Image.FromFile(dialog.FileName);
+                PBBefore.Image = image;
+            }
+            catch (OutOfMemoryException)
+            {
+                MessageBox.Show(@"Неверный формат изображения. Возможно, изображение повреждено.");
+            }
+        }
+
+        [SuppressMessage("ReSharper", "InconsistentNaming")]
+        private void ButtonProcess_Click(object sender, EventArgs e)
+        {
+            #region Declaration of locals
+
+            var bmapInit   = PBBefore.Image as Bitmap; // только для чтения здесь
+            var bmapResult = new Bitmap(bmapInit.Width, bmapInit.Height);
+            var tablePixelTuples =
+                new (byte R, byte G, byte B)[bmapInit.Width, bmapInit.Height]; // Обращение: [x, y]  >^
+            // Такс, кортеж - что-то вроде литерала
+            int Rmin = 0,
+                Rmax = 0;
+            int Gmin = 0,
+                Gmax = 0;
+            int Bmin = 0,
+                Bmax = 0;
+            var GistogramR = new List<(int X, int Y)>[256]; //от 0 до 255 включительно
+            var GistogramG = new List<(int X, int Y)>[256];
+            var GistogramB = new List<(int X, int Y)>[256];
+            var ResultsR   = new byte[256]; //Значение функции будет от 0 до 255
+            var ResultsG   = new byte[256]; //Значение функции будет от 0 до 255
+            var ResultsB   = new byte[256]; //Значение функции будет от 0 до 255
+
+            #endregion
+
+            InitLocals();
+
+            Init_GistogramsThenMaxesAndMins();
+
+            #region Init arrays of results
+
+            double multiplierR = 255 / (double) (Rmax - Rmin);
+            double multiplierG = 255 / (double) (Gmax - Gmin);
+            double multiplierB = 255 / (double) (Bmax - Bmin);
+
+            Init_Arrays_Of_Results();
+
+            #endregion
+
+            #region Init table of result
+            // в гистограммах у меня находятся точки. В массивах результатов у меня значение для каждого столбца. 
+            // я прохожу по гистограмме, и для каждой точки в столбце ставлю R или G или B из массива результата.
+
+
+            #endregion
+
+
+            void InitLocals()
+            {
+                for (var i = 0; i < GistogramR.Length; i++)
+                {
+                    GistogramR[i] = new List<(int X, int Y)>(bmapInit.Width);
+                }
+
+                for (var i = 0; i < GistogramG.Length; i++)
+                {
+                    GistogramG[i] = new List<(int X, int Y)>(bmapInit.Width);
+                }
+
+                for (var i = 0; i < GistogramB.Length; i++)
+                {
+                    GistogramB[i] = new List<(int X, int Y)>(bmapInit.Width);
+                }
+            }
+
+            void Init_GistogramsThenMaxesAndMins()
+            {
+                for (var j = 0; j < bmapInit.Height; j++)
+                {
+                    for (var i = 0; i < bmapInit.Width; i++)
+                    {
+                        var c = bmapInit.GetPixel(i, j);
+                        tablePixelTuples[i, j] = (c.R, c.G, c.B); // [x,y] = color
+                        GistogramR[c.R]
+                            .Add((i, j)); // так, мне нужно добавить тут именно 2д точку, т.е. xy, т.е. то же, что в цвете.
+                        GistogramG[c.G].Add((i, j));
+                        GistogramB[c.B].Add((i, j));
+                    }
+                }
+
+                for (var i = 0; i < GistogramR.Length; i++)
+                {
+                    if (GistogramR[i].Count > 0)
+                    {
+                        Rmin = i;
+                        break;
+                    }
+                }
+
+                for (var i = GistogramR.Length - 1; i >= 0; i--)
+                {
+                    if (GistogramR[i].Count > 0)
+                    {
+                        Rmax = i;
+                        break;
+                    }
+                }
+
+                for (var i = 0; i < GistogramG.Length; i++)
+                {
+                    if (GistogramG[i].Count > 0)
+                    {
+                        Gmin = i;
+                        break;
+                    }
+                }
+
+                for (var i = GistogramG.Length - 1; i >= 0; i--)
+                {
+                    if (GistogramG[i].Count > 0)
+                    {
+                        Gmax = i;
+                        break;
+                    }
+                }
+
+                for (var i = 0; i < GistogramB.Length; i++)
+                {
+                    if (GistogramB[i].Count > 0)
+                    {
+                        Bmin = i;
+                        break;
+                    }
+                }
+
+                for (var i = GistogramB.Length - 1; i >= 0; i--)
+                {
+                    if (GistogramB[i].Count > 0)
+                    {
+                        Bmax = i;
+                        break;
+                    }
+                }
+            }
+
+            void Init_Arrays_Of_Results()
+            {
+                for (var i = 0; i < ResultsR.Length; i++)
+                {
+                    if (GistogramR[i].Count is 0)
+                    {
+                        return;
+                    }
+
+                    var x      = tablePixelTuples[GistogramR[i][0].X, GistogramR[i][0].Y].R;
+                    var result = (int) ((x - Rmin) * multiplierR);
+                    if (result > 255 ||
+                        result < 0)
+                    {
+                        throw new ArgumentException("результат формулы не вмещается в байт");
+                    }
+
+                    ResultsR[i] =
+                        (byte) result; // хм. X не может быть меньше минимального значения. Может, все и в порядке.
+                }
+
+                for (var i = 0; i < ResultsG.Length; i++)
+                {
+                    if (GistogramG[i].Count is 0)
+                    {
+                        return;
+                    }
+
+                    var x      = tablePixelTuples[GistogramG[i][0].X, GistogramG[i][0].Y].G;
+                    var result = (int) ((x - Gmin) * multiplierG);
+                    if (result > 255 ||
+                        result < 0)
+                    {
+                        throw new ArgumentException("результат формулы не вмещается в байт");
+                    }
+
+                    ResultsG[i] =
+                        (byte) result; // хм. X не может быть меньше минимального значения. Может, все и в порядке.
+                }
+
+                for (var i = 0; i < ResultsB.Length; i++)
+                {
+                    if (GistogramB[i].Count is 0)
+                    {
+                        return;
+                    }
+
+                    var x      = tablePixelTuples[GistogramB[i][0].X, GistogramB[i][0].Y].B;
+                    var result = (int) ((x - Bmin) * multiplierB);
+                    if (result > 255 ||
+                        result < 0)
+                    {
+                        throw new ArgumentException("результат формулы не вмещается в байт");
+                    }
+
+                    ResultsB[i] =
+                        (byte) result; // хм. X не может быть меньше минимального значения. Может, все и в порядке.
+                }
+            }
+        }
+    }
+
+    struct StructForType
+    {
+        public byte R;
+        public byte G;
+        public byte B;
     }
 }
